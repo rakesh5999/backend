077afb0e673253872ec95a47b85a2ba93d77642b
rakeshgowda1512-a11y <rakeshgowda1512@gmail.com>
2026-05-10 19:46:39 +0530

    UI: Remove share icon and ensure profile stats visibility

--- day-15/frontend/src/features/posts/components/Post.jsx
+++ day-15/frontend/src/features/posts/components/Post.jsx
@@ -71,12 +71,6 @@ const Post = ({ user, post, loading, handleLike, handleUnLike, handleFollow, han
             </svg>
           </button>
 
-          <button>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinejoin="round">
-              <line x1="22" y1="2" x2="11" y2="13"></line>
-              <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
-            </svg>
-          </button>
         </div>
 
         <div className="right">
